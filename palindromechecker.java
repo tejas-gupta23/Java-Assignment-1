9403e8e345d5c6196ea6dea785ce55228cd7f648
Tejas Gupta <125728892+tejas-gupta23@users.noreply.github.com>
2023-03-16 22:57:31 +0530

    Create palindromechecker.java

---
+++ palindromechecker.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class palindromechecker {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        if (isPalindrome(num)) {
+            System.out.println(num + " is a palindrome number.");
+        } else {
+            System.out.println(num + " is not a palindrome number.");
+        }
+    }
+
+    public static boolean isPalindrome(int num) {
+        int reversed = 0;
+        int original = num;
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+        return original == reversed;
+    }
+}
